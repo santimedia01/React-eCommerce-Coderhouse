44b31d5d055a4bfcf129442dbc95b78af88a6781
Santiago Mediabilla <santiagomediabilla@gmail.com>
2020-08-05 10:09:58 -0300

    Initialize gh-pages

---
+++ precache-manifest.d380b70daec09b8d23d8c5ccd907c3fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f1dbf0afd9ec8436566c7d3e3f4ef1",
+    "url": "/React-ReactRouter-MaterialUI-Template/index.html"
+  },
+  {
+    "revision": "11f6347490cb9fa35b70",
+    "url": "/React-ReactRouter-MaterialUI-Template/static/css/main.ff862c72.chunk.css"
+  },
+  {
+    "revision": "97c52fe0154128547575",
+    "url": "/React-ReactRouter-MaterialUI-Template/static/js/2.cbfe10e2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-ReactRouter-MaterialUI-Template/static/js/2.cbfe10e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11f6347490cb9fa35b70",
+    "url": "/React-ReactRouter-MaterialUI-Template/static/js/main.62b30bea.chunk.js"
+  },
+  {
+    "revision": "61c28e26af29100e6ca7",
+    "url": "/React-ReactRouter-MaterialUI-Template/static/js/runtime-main.4d9181b5.js"
+  },
+  {
+    "revision": "c0a4937ea67acc89c7beac19d5dd67c5",
+    "url": "/React-ReactRouter-MaterialUI-Template/static/media/404.c0a4937e.png"
+  }
+]);
